cfd9ca79e3cbdb942d1095916fec27f65656d32c
bri <briegrant416@gmail.com>
2018-04-25 19:50:48 +0300

    Add templates index.html,navbar.html,urls.py and views.py details

---
+++ transact/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from . forms import ProfileForm,ProfileUploadForm
+from django.http  import HttpResponse
+from . models import Pic 
+from django.conf import settings
+
+
+# Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+      title = 'Transaction'
+      pic_posts = Pic.objects.all()
+      
+      print(pic_posts)
+      return render(request, 'index.html', {"title":title,"pic_posts":pic_posts})
+
+
+@login_required(login_url='/accounts/login/')
+def upload_profile(request):
+    current_user = request.user 
+    title = 'Upload Profile'
+    try:
+        requested_profile = Profile.objects.get(user_id = current_user.id)
+        if request.method == 'POST':
+            form = ProfileUploadForm(request.POST,request.FILES)
+
+            if form.is_valid():
+                requested_profile.profile_pic = form.cleaned_data['profile_pic']
+                requested_profile.bio = form.cleaned_data['bio']
+                requested_profile.username = form.cleaned_data['username']
+                requested_profile.save_profile()
+                return redirect( profile )
+        else:
+            form = ProfileUploadForm()
+    except:
+        if request.method == 'POST':
+            form = ProfileUploadForm(request.POST,request.FILES)
+
+            if form.is_valid():
+                new_profile = Profile(profile_pic = form.cleaned_data['profile_pic'],bio = form.cleaned_data['bio'],username = form.cleaned_data['username'])
+                new_profile.save_profile()
+                return redirect( profile )
+        else:
+            form = ProfileUploadForm()
+
+
+    return render(request,'upload_profile.html',{"title":title,"current_user":current_user,"form":form})
